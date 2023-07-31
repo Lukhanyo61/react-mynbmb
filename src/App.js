9625e3a7db3b227f3366c313db2ac26f1a331569
Lukhanyo61 <105869393+Lukhanyo61@users.noreply.github.com>
2023-07-31 17:41:26 +0200

    4 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Index from './Pages/Index'
+import About from './Pages/About';
+import Beneficiary from './Pages/Beneficiary';
+import Contact from './pages/contact';
+import './App.css'
+ 
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path='/' exact element={<Index />} />
+                <Route path='/About' element={<About />} />
+                <Route path='/Contact' element={<Contact />} />
+                <Route path='/Beneficary' element={<Beneficiary />} />
+                
+            </Routes>
+        </Router>
+    );
 }
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-
+ 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
@@ -10,4 +11,12 @@ root.render(
   <StrictMode>
     <App />
   </StrictMode>
+);
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
